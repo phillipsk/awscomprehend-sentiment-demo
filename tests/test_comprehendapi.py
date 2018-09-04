76a53e26cd8c2dce2fd1605cd0345edac9a8ee7e
aeg <aeg@amazon.com>
2018-09-04 19:08:36 +1000

    Add unit tests

--- tests/test_comprehendapi.py
+++ tests/test_comprehendapi.py
@@ -1,30 +1,74 @@
 from unittest import TestCase
+from unittest.mock import MagicMock
 
 from comprehendApi import ComprehendApi
+from unittest.mock import patch
 
 
-class ITTestComprehendapi(TestCase):
+class TestComprehendapi(TestCase):
     def test_get_sentiment_singledoc(self):
         # Arrange
-        sut = ComprehendApi()
+        data = "This is bad"
+        mock_client = MagicMock()
+        expected_sentiment = "negative"
+        mock_client.detect_sentiment.return_value = {'Sentiment': 'NEGATIVE',
+                                                     'SentimentScore': {
+                                                         'Positive': 0,
+                                                         'Negative': 1.0,
+                                                         'Neutral': 0,
+                                                         'Mixed': 0
+                                                     }}
+        sut = ComprehendApi(mock_client)
 
         # Act
-        actual_sentiment, confidence_score = sut.get_sentiment_singledoc("This is bad")
-        expected_sentiment = "negative"
+
+        actual_sentiment, confidence_score = sut.get_sentiment_singledoc(data, 'es')
 
         # Assert
         self.assertEqual(actual_sentiment.lower(), expected_sentiment)
-        self.assertGreater(confidence_score, 0)
+        self.assertEqual(confidence_score, 1.0)
 
     def test_get_sentiment_batch(self):
         # Arrange
-        sut = ComprehendApi()
         data = ["This is bad"
             , "This is good"]
+        mock_client = MagicMock()
+
+        # Response looks like this for batch sentiment
+
+        mock_client.batch_detect_sentiment.return_value =  {
+            'ResultList': [
+                {
+                    'Index': 1,
+                    'Sentiment': 'POSITIVE' ,
+                    'SentimentScore': {
+                        'Positive': 1.0,
+                        'Negative': 0.0,
+                        'Neutral': 0.0,
+                        'Mixed': 0.0
+                    }
+                },
+                {
+                    'Index': 0,
+                    'Sentiment': 'NEGATIVE',
+                    'SentimentScore': {
+                        'Positive': 0.1,
+                        'Negative': .8,
+                        'Neutral': 0.1,
+                        'Mixed': 0.0
+                    }
+                },
+            ],
+            'ErrorList': [
+
+            ]
+        }
+
+
+        sut = ComprehendApi(mock_client)
 
         # Act
         actual = sut.get_sentiment_batch(data)
 
         # Assert
         self.assertEqual(len(data), len(actual))
-
