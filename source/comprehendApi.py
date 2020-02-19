c0698bd73b488f633ef418e43d187cfe48ce4379
kevinphillips <inquiries@kevinphillips.email>
2020-02-18 23:42:12 -0500

    begin refactored for all AWS Comprehend services

--- source/comprehendApi.py
+++ source/comprehendApi.py
@@ -7,8 +7,21 @@ from itertools import product
 
 class ComprehendApi:
 
-    def __init__(self, client=None):
+    def __init__(self, aws_service, client=None):
         self.client = client or boto3.client('comprehend')
+        self.get_aws_service(aws_service)
+
+    def get_aws_service(self, aws_service):
+        if aws_service in 'batch_detect_dominant_language':
+            self.aws_service = 'det_lang'
+        if aws_service in 'batch_detect_entities':
+            self.aws_service = 'det_entities'
+        if aws_service in 'batch_detect_key_phrases':
+            self.aws_service = 'det_phrases'
+        if aws_service in 'batch_detect_sentiment':
+            self.aws_service = 'det_sent'
+        if aws_service in 'batch_detect_syntax':
+            self.aws_service = 'det_syntax'
 
     def get_sentiment_singledoc(self, text: str, language_code: str = 'en') -> tuple:
         """
@@ -24,7 +37,7 @@ Obtains sentiment for a single text
 
         return sentiment, confidence_score
 
-    def get_sentiment_batch_bulk(self, list_of_doc: list, language_code: str = 'en', max_threads=10) -> list:
+    def get_comprehend_batch_bulk(self, list_of_doc: list, language_code: str = 'en', max_threads=10) -> list:
         """
 Obtains sentiment for a batch of documents. Comprehend api only accepts 25  documents at time, so this splits a list into chunks .
         :param list_of_doc: list of documents
@@ -32,33 +45,55 @@ Obtains sentiment for a batch of documents. Comprehend api only accepts 25  docu
         :return: a list of sentiments
         """
         pool = ThreadPool(max_threads)
-        result = pool.starmap(self.get_sentiment_batch,
+        result = pool.starmap(self.get_comprehend_batch,
                               product(self._chunks(list_of_doc, chunk_size=20), [language_code]))
         pool.close()
         pool.join()
         result = reduce(lambda x, y: x + y, result)
         return result
 
-    def get_sentiment_batch(self, list_of_doc, language_code):
+    def get_comprehend_batch(self, list_of_doc, language_code):
         # Comprehend api only accepts 25  documents at time
         assert len(list_of_doc) <= 25
 
-        response = self.client.batch_detect_sentiment(TextList=list_of_doc, LanguageCode=language_code)
-        # Error handling
-        error_list = response['ErrorList']
-        if len(error_list) > 0:
-            error_msg = '\n'.join(['{} {}'.format(e['Index'], e['ErrorMessgae']) for e in error_list])
-            raise RuntimeError(error_msg)
-        # Collect results
-        result = []
-        for r in response['ResultList']:
-            input_index = r['Index']
-            text = list_of_doc[input_index]
-            sentiment = r["Sentiment"]
-            confidence_score = r["SentimentScore"][sentiment.title()]
+        if self.aws_service in "det_sent":
+            response = self.client.batch_detect_sentiment(TextList=list_of_doc, LanguageCode=language_code)
+            # response = self.client.batch_detect_key_phrases(TextList=list_of_doc, LanguageCode=language_code)
+            # Error handling
+            error_list = response['ErrorList']
+            if len(error_list) > 0:
+                error_msg = '\n'.join(['{} {}'.format(e['Index'], e['ErrorMessage']) for e in error_list])
+                raise RuntimeError(error_msg)
+            # Collect results
+            result = []
+            for r in response['ResultList']:
+                input_index = r['Index']
+                text = list_of_doc[input_index]
+                sentiment = r["Sentiment"]
+                confidence_score = r["SentimentScore"][sentiment.title()]
 
-            result.append((text, sentiment, confidence_score))
-        return result
+                result.append((text, sentiment, confidence_score))
+            return result
+        elif self.aws_service in 'det_phrases':
+            response = self.client.batch_detect_key_phrases(TextList=list_of_doc, LanguageCode=language_code)
+            # Error handling
+            error_list = response['ErrorList']
+            if len(error_list) > 0:
+                error_msg = '\n'.join(['{} {}'.format(e['Index'], e['ErrorMessage']) for e in error_list])
+                raise RuntimeError(error_msg)
+            # Collect results
+            result = []
+            for r in response['ResultList']:
+                if len(r['KeyPhrases']) == 0:
+                    continue
+                else:
+                    input_index = r['Index']
+                    text = list_of_doc[input_index]
+                    score = r['KeyPhrases'][0]['Score']
+                    keyText = r['KeyPhrases'][0]['Text']
+
+                    result.append((text, keyText, score))
+            return result
 
     def _chunks(self, list, chunk_size=20):
 
--- source/demo.py
+++ source/demo.py
@@ -5,7 +5,7 @@ import pprint
 
 import sys
 
-from comprehendApi import ComprehendApi
+from source.comprehendApi import ComprehendApi
 
 MODE_SINGLE = 'single'
 MODE_BATCH = 'batch'
@@ -23,7 +23,7 @@ def write_result(result, outfile=None, encoding="utf-8"):
     print("The results have been saved to {}".format(outfile))
 
 
-def run_demo(mode, input, outfile=None, max_threads=1, encoding="utf-8"):
+def run_demo(mode, aws_service, input, outfile=None, max_threads=1, encoding="utf-8"):
     """
 Runs a sample demo
     :param mode: Mode is either single or batch
@@ -41,52 +41,58 @@ Runs a sample demo
         result = api.get_sentiment_singledoc(input)
 
     elif mode == MODE_BATCH:
-        result = run_batch_demo(input, encoding, max_threads)
+        result = run_batch_demo(input, encoding, max_threads, aws_service)
 
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(result)
     write_result(result, outfile)
 
 
-def run_batch_demo(input, encoding, max_threads):
+def run_batch_demo(input, encoding, max_threads, aws_service):
     input_list = []
-    api = ComprehendApi()
+    api = ComprehendApi(aws_service)
     # read file
     with open(input, "r", encoding=encoding) as f:
         for line in f:
             input_list.append(line.strip('\n'))
-    result = api.get_sentiment_batch_bulk(input_list, max_threads=max_threads)
+    result = api.get_comprehend_batch_bulk(input_list, max_threads=max_threads)
     return result
 
 
 if __name__ == '__main__':
-    mode = [MODE_SINGLE, MODE_BATCH]
-    parser = argparse.ArgumentParser()
-    parser.add_argument("mode",
-                        help="The mode of input", choices=[MODE_SINGLE, MODE_BATCH])
+    run_demo("batch", "phrases", "../TXT_asrOutput.txt", "../keyPhrases.csv", 3)
 
-    parser.add_argument("input",
-                        help="Single sentence for single mode. For batch mode provide an input file")
+#     mode = [MODE_SINGLE, MODE_BATCH]
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("mode",
+#                         help="The mode of input", choices=[MODE_SINGLE, MODE_BATCH])
+#
+#     parser.add_argument("service",
+#                         help="The desired AWS Comprehend service to analyze input", choices=[MODE_SINGLE, MODE_BATCH])
+#
+#     parser.add_argument("input",
+#                         help="Single sentence for single mode. For batch mode provide an input file")
+#
+#     parser.add_argument("--outfile",
+#                         help="Saves the results to the output file. Only relevant for batch mode")
+#
+#     parser.add_argument("--max-threads",
+#                         help="This is relevant only for {} mode. The number of threads used to process the requests".format(
+#                             MODE_BATCH), type=int, default=1)
+#
+#     parser.add_argument("--encoding",
+#                         help="This is relevant only for {} mode. The number of threads used to process the requests".format(
+#                             MODE_BATCH), type=str, default="utf-8")
+#
+#     parser.add_argument("--loglevel",
+#                         help="This is the log level", type=str, default="INFO", choices={"INFO", "DEBUG", "WARN", "ERROR"})
+#
+    # args = parser.parse_args()
+#
+#     #Set up logging
+#     logging.basicConfig(level=logging.getLevelName(args.loglevel), handlers=[logging.StreamHandler(sys.stdout)],
+#                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-    parser.add_argument("--outfile",
-                        help="Saves the results to the output file. Only relevant for batch mode")
 
-    parser.add_argument("--max-threads",
-                        help="This is relevant only for {} mode. The number of threads used to process the requests".format(
-                            MODE_BATCH), type=int, default=1)
-
-    parser.add_argument("--encoding",
-                        help="This is relevant only for {} mode. The number of threads used to process the requests".format(
-                            MODE_BATCH), type=str, default="utf-8")
-
-    parser.add_argument("--loglevel",
-                        help="This is the log level", type=str, default="INFO", choices={"INFO", "DEBUG", "WARN", "ERROR"})
-
-    args = parser.parse_args()
-
-    #Set up logging
-    logging.basicConfig(level=logging.getLevelName(args.loglevel), handlers=[logging.StreamHandler(sys.stdout)],
-                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-
-    run_demo(args.mode, args.input, args.outfile, args.max_threads, args.encoding)
+    # run_demo(args.mode, args.input, args.outfile, args.max_threads, args.encoding)
+    # run_demo(args.mode, , args.outfile, args.max_threads, args.encoding)
